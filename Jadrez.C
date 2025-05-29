7e7b7970ef136e80b3d2f09bd5a9d6fdb47f386e
Vanderleia <vanderleiasantosdaconceicaosan@gmail.com>
2025-05-29 12:40:35 +0000

    Jogo de Jadrez

--- Jadrez.C
+++ Jadrez.C
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+printf("Desafio de Xadrez - MateCheck");
+
+int main() {
+
+   //Movimentos do Bispo
+   for (int i = 0; i < 5; i++) {
+    printf("Direita\n"); // Imprimir a direita da movimento 
+   }   
+ return 0;
+   }
