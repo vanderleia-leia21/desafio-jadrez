24fabe0a41005b645c8097d85750affcfae1459e
Vanderleia <vanderleiasantosdaconceicaosan@gmail.com>
2025-05-29 13:05:49 +0000

    criando o movimento do bispo

--- Jadrez.C
+++ Jadrez.C
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-printf("Desafio de Xadrez - MateCheck");
-
 int main() {
 
    //Movimentos do Bispo
-   for (int i = 0; i < 5; i++) {
+   for (int i = 0; i <= 5; i++) {
     printf("Direita\n"); // Imprimir a direita da movimento 
    }   
+
+while(int enum = 0; enum <= 5) { 
+    printf("cima\n"&enum); //"""Imprimir a cima para movimento do bispo"""
+}
+
+
+
  return 0;
    }
