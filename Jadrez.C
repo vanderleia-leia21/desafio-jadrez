7fc5f184cb3a360c68edfdaea966ce6c9cbe7fc9
Vanderleia <vanderleiasantosdaconceicaosan@gmail.com>
2025-05-29 13:51:34 +0000

    Quase concluindo

--- Jadrez.C
+++ Jadrez.C
@@ -3,35 +3,39 @@
 int main() {
     int x = 0, y = 0; // Posição inicial comum
 
-    // ===== Movimento do Bispo (diagonal superior direita - x++, y++) usando for =====
+    // ===== Movimento do Bispo (diagonal superior direita x++, y++) usando for =====
     printf("Movimento do Bispo:\n");
     for (int i = 0; i < 5; i++) {
         x++;
         y++;
-        printf("Cima: %d\n", x, y);
+        printf("\nCima/esquer: %dcasa\n", x, y);
+        printf("cima/direita:%dcasa\n", x,y);
+        printf("baixo/esquerda:%ddcasa\n", x,y);
+        printf("baixo/direita:%dcasa\n", x,y);
+    
     }
 
     // Reiniciando posição
     x = 0;
 
-    // ===== Movimento da Torre (direita - x++) usando while =====
+    // ===== Movimento da Torre (direita x++) usando while =====
     printf("\nMovimento da Torre :\n");
     int i = 0;
     while (i < 5) {
         x++;
-        printf("direita: %d\n", x, y);  // y permanece o mesmo
+        printf("\ndireita: %d casa\n", x, y);  // y permanece o mesmo
         i++;
     }
 
     // Reiniciando posição
     x = 0;
 
-    // ===== Movimento da Rainha (esquerda - x--) usando do-while =====
+    // ===== Movimento da Rainha (esquerda x) usando do-while =====
     printf("\nMovimento da Rainha:\n");
     i = 0;
     do {
-        x--;
-        printf("Direita: %d\n", x, y);  // y permanece o mesmo
+        x++;
+        printf("\nEsquerda: %d casa\n", x, y);  // y permanece o mesmo
         i++;
     } while (i < 8);
 
